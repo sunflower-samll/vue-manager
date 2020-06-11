85d9335f1431d4960d214cd99c521fada4d0b3aa
sunflower-small <345361470@qq.com>
2020-06-11 23:05:12 +0800

    瓦城

--- src/pages/index/product/Product-add.vue
+++ src/pages/index/product/Product-add.vue
@@ -107,7 +107,7 @@ export default {
     //查询所有商品分类名称
       API_product_cate().then(res=>{
         // console.log(res.data.categories)
-        // console.log(res.data.categories)
+        // console.log(res11111.data.categories)
         // console.log(res.data.categories)
         // console.log(res.data.categories)
         this.categories=res.data.categories
